623c1ae072675647479f57ba29372292e5590139
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2023-06-26 17:14:35 -0500

    feat: add SessionProvider to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 
 import '@/styles/globals.css';
 import Layout from '@/components/Layout';
@@ -7,13 +8,13 @@ import RegisterModal from '@/components/modals/RegisterModal';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <LoginModal />
       <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   );
 }
 
