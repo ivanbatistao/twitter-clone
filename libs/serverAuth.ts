7af0a1f765bd17023e624cd64429e0eca08b65d6
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2023-06-26 17:04:34 -0500

    fix: change import route of authOptions - next auth

--- libs/serverAuth.ts
+++ libs/serverAuth.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from './prismadb';
 import { getServerSession } from 'next-auth';
-import { authOptions } from "@/pages/api/auth/[...next]";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 
 const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
