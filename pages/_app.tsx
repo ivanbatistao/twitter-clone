e916db8573dd55b207418b3def358e1da3195a4b
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2023-06-26 09:20:07 -0500

    feat: add register component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import Layout from '@/components/Layout';
 import LoginModal from '@/components/modals/LoginModal';
+import RegisterModal from '@/components/modals/RegisterModal';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <LoginModal />
+      <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
